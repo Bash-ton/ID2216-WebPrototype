92a33065767b1cb5a4872f52a5bf99aed03e4da1
mohamed <mohamed4mahdi@gmail.com>
2021-02-02 15:02:03 +0100

    few components done

--- src/components/scheduleType.js
+++ src/components/scheduleType.js
@@ -1,9 +1,22 @@
 import React, {Component} from "react";
-
+import "../css/scheduleType.css"
 class ScheduleType extends Component{
+
+   detailedschedule =e=>{
+        this.props.history.push("/detailed-schedule")
+     }
+     premadeschedule =e=>{
+        this.props.history.push("/premade-scheduletype")
+     }
     render(){
         return(
-            <div>ScheduleType</div>
+            <div class="v160_86">
+                <div class="name"></div> 
+           
+                <div class="v160_96"onClick={this.premadeschedule}></div>
+                <div class="v160_117"onClick={this.detailedschedule}></div>
+                <span class="v160_97" onClick={this.detailedschedule}>Create Your Own schedule </span>
+                <span class="v160_98"onClick={this.premadeschedule}>Pick an existing Scheduling</span></div>
         );
     }
 
--- src/components/signInPage.js
+++ src/components/signInPage.js
@@ -1,9 +1,25 @@
 import React, {Component} from "react";
-
+import "../css/styles.css"
 class SinginPage extends Component{
+
+
+    signup =e=>{
+        this.props.history.push("/tutorial")
+     }
+ 
+    login=e=>{
+       this.props.history.push("/")
+    }
     render(){
         return(
-            <div>SinginPage</div>
+            <div class="v120_25">
+            <div class="v120_26"></div>
+            <span class="v120_27">Sign up</span>
+            <input class="v120_28"></input>
+            <input class="v120_29"></input>
+            <button class="v120_30"onClick={this.login}>Log in</button>
+            <button class="v120_38" onClick={this.signup}>Sign up</button>
+        </div>
         );
     }
 
