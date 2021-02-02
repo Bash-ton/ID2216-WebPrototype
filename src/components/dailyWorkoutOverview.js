881b880eca0e87a064c7cf7e85dc6ea627233bfc
Bash-ton <sebbepaz@gmail.com>
2021-02-02 14:59:31 +0100

    daylyoverview static done

--- src/components/dailyWorkoutOverview.js
+++ src/components/dailyWorkoutOverview.js
@@ -1,12 +1,39 @@
-import React, {Component} from "react";
+import React from "react";
+import './css/daliyWorkoutOverview.css'
 
+const DailyWorkoutOverview = () =>{
 
-class DailyWorkoutOverview extends Component{
-    render(){
-        return(
-            <div>DailyWorkoutOverview</div>
-        );
-    }
+    return(
+        <div className="v120_44">
+            <span className="v120_50">What to do</span>
+            <span className="v120_51">2021-01-25</span>
+            <div className="v142_2"></div>
+            <span className="v142_3">ÖvningNamn</span>
+            <div className="v142_6"></div>
+            <div className="name"></div>
+            <div className="v151_22"></div>
+            <div className="v151_78"></div>
+            <span className="v151_79">ÖvningNamn</span>
+            <div className="v151_80"></div>
+            <div className="v151_81"></div>
+            <div className="v151_74"></div>
+            <span className="v151_75">ÖvningNamn</span>
+            <div className="v151_76"></div>
+            <div className="v151_77"></div>
+            <div className="name"></div>
+            <div className="v151_413"></div>
+            <div className="v151_414"></div>
+            <div className="v151_415"></div>
+            <div className="v151_416"></div>
+            <div className="v196_1"></div>
+            <span className="v196_2">mark all </span>
+            <div className="v242_2"></div>
+            <div className="v242_3"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+            <span className="v242_6">Välj pass</span>
+        </div>
+    );
 
 }
 export default DailyWorkoutOverview;
