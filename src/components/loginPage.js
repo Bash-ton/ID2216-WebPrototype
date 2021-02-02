39dfb9c35f44b39c65a8f341bdd49efefd40e143
mohamed <mohamed4mahdi@gmail.com>
2021-02-02 11:30:39 +0100

    figma html & css, routing

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -1,10 +1,26 @@
 import React, {Component} from "react";
-
+import "../styles.css"
 
 class LoginPage extends Component{
+
+    handleClick=e=>{
+       this.props.history.push("/daily-workout-overview")
+    }
+
+
     render(){
-        return(
-            <div>Loginpage</div>
+        return(  
+            <div class="v120_25">
+                <div class="v120_26"></div>
+                <span class="v120_27">Log in</span>
+                <div class="v120_28"></div>
+                <span class="v120_37">*******</span>
+                <div class="v120_29"></div>
+                <span class="v120_35">balbalba@test.se</span>
+                <button class="v120_30">Sign up</button>
+                <button class="v120_38" onClick={this.handleClick}>login</button>
+            </div>
+
         );
     }
 
