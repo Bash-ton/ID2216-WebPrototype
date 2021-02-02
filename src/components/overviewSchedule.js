160eb69d1f10d4edb53aa7e25e16210e95a1ad98
Bash-ton <sebbepaz@gmail.com>
2021-02-02 14:15:51 +0100

    overviewSchedule done

--- src/components/overviewSchedule.js
+++ src/components/overviewSchedule.js
@@ -1,11 +1,38 @@
-import React, {Component} from "react";
+import React from "react";
+import './css/overviewSchedule.js.css'
 
-class OvervieSchedule extends Component{
-    render(){
+const OverviewSchedule = () => {
+
+        const handleAddSchedule = () =>{
+            window.location = '/detailed-schedule';
+        }
+
         return(
-            <div>overvieSchedule</div>
+            <div className="v180_13">
+                    <div className="name"></div>
+
+                    <div className="v180_17"></div>
+                    <div className="v180_18"></div>
+                    <div className="v180_19"></div>
+                    <div className="v180_20"></div>
+
+                    <div className="v180_21"></div>
+                    <div onClick={() =>{handleAddSchedule()}} className="v180_22"></div>
+                    <div className="v180_23"></div>
+                    <div className="v180_24"></div>
+                    <div className="v180_25"></div>
+                    <span className="v180_26">schedule 2</span><span className="v180_27">schedule 1</span>
+                    <div className="v180_28"></div>
+                    <div className="v180_29"></div>
+                    <span className="v180_30">schedul 3</span>
+                    <span className="v180_31">Choose active schedule</span>
+                    <span className="v180_32">schedule 4</span>
+                    <div className="v180_33"></div>
+                    <div className="v180_34"></div>
+                    <div className="v180_35"></div>
+            </div>
         );
-    }
+
 
 }
-export default OvervieSchedule;
+export default OverviewSchedule;
